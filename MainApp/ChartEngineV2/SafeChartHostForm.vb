4ea0da46725b59e2c8330a9a7f4be8954b2e36fc
hoonoh57 <36817134+hoonoh57@users.noreply.github.com>
2026-05-01 11:38:04 +0900

    Make V2 host use original chart host wiring

--- MainApp/ChartEngineV2/SafeChartHostForm.vb
+++ MainApp/ChartEngineV2/SafeChartHostForm.vb
@@ -3,184 +3,153 @@ Option Explicit On
 Option Infer Off
 
 Imports System
-Imports System.Reflection
+Imports System.Drawing
+Imports System.IO
 Imports System.Windows.Forms
+Imports [Shared]
+Imports MainApp.Models
 Imports MainApp.Services
 
 Public Class SafeChartHostForm
     Inherits Form
+    Implements IChartHost
 
     Private ReadOnly _chart As SafeFastChartControl
-    Private ReadOnly _stockCode As String
+    Private _stockCode As String = ""
+    Private ReadOnly _initialStockCode As String
     Private ReadOnly _chartType As String
     Private ReadOnly _count As Integer
 
     Public Sub New(stockCode As String, Optional chartType As String = "minute", Optional count As Integer = 300)
-        _stockCode = stockCode
+        _initialStockCode = SharedUtil.NormalizeChartCode(stockCode)
         _chartType = chartType
         _count = count
 
-        Me.Text = "Safe Chart V2 - " & stockCode
+        Me.Text = "Safe Chart V2 - " & _initialStockCode
         Me.Width = 1200
         Me.Height = 800
         Me.StartPosition = FormStartPosition.CenterScreen
 
         _chart = New SafeFastChartControl()
         _chart.Dock = DockStyle.Fill
+        _chart.SetHost(Me)
         Me.Controls.Add(_chart)
 
-        InstallOriginalStyleContextMenu()
-
+        AddHandler _chart.IndicatorSettingRequested, AddressOf OnIndicatorSettingRequested
         AddHandler _chart.ChartProfileChanged, AddressOf OnChartProfileChanged
+        AddHandler _chart.StrategySettingRequested, AddressOf OnStrategySettingRequested
+        AddHandler _chart.DataViewRequested, AddressOf OnDataViewRequested
         AddHandler Me.Shown, AddressOf OnSafeChartHostShown
     End Sub
 
     Private Sub OnSafeChartHostShown(sender As Object, e As EventArgs)
-        _chart.ApplyChartProfile(ChartProfileService.I.GetProfile())
+        Dim profilePath As String = Path.Combine(Application.StartupPath, "chart_profile.json")
+        If File.Exists(profilePath) Then
+            _chart.ApplyChartProfile(ChartProfileService.I.GetProfile())
+        End If
+        SetStock(_initialStockCode)
+    End Sub
+
+    Public Sub SetStock(stockCode As String)
+        _stockCode = SharedUtil.NormalizeChartCode(stockCode)
+        Dim name As String = GetStockName(_stockCode)
+        Me.Text = "Safe Chart V2 - [" & _stockCode & "] " & name
         _chart.SetStock(_stockCode, _chartType, _count)
     End Sub
 
+    Private Sub OnIndicatorSettingRequested(sender As Object, e As EventArgs)
+        Dim ind As IIndicator = _chart.GetSelectedIndicator()
+        If ind Is Nothing Then Return
+
+        Using f As New IndicatorSettingForm(ind)
+            If f.ShowDialog(Me) = DialogResult.OK Then
+                _chart.ReCalculate()
+                SaveChartProfile()
+            End If
+        End Using
+    End Sub
+
     Private Sub OnChartProfileChanged(sender As Object, e As EventArgs)
+        SaveChartProfile()
+    End Sub
+
+    Private Sub SaveChartProfile()
         ChartProfileService.I.SaveProfile(_chart.ExportChartProfile())
     End Sub
 
-    Private Sub InstallOriginalStyleContextMenu()
-        Dim menu As ContextMenuStrip = BuildOriginalStyleContextMenu()
-        _chart.ContextMenuStrip = menu
-
-        Dim field As FieldInfo = GetType(SafeFastChartControl).GetField("_sk", BindingFlags.Instance Or BindingFlags.NonPublic)
-        If field Is Nothing Then Return
-
-        Dim skControl As Control = TryCast(field.GetValue(_chart), Control)
-        If skControl IsNot Nothing Then
-            skControl.ContextMenuStrip = menu
-        End If
+    Private Sub OnStrategySettingRequested(sender As Object, e As EventArgs)
+        Using f As New StrategyManagerForm(AddressOf ApplyStrategyDefinitionAndAnalyze,
+                                           AddressOf ApplyHardcodedStrategyAndAnalyze)
+            f.ShowDialog(Me)
+        End Using
     End Sub
 
-    Private Function BuildOriginalStyleContextMenu() As ContextMenuStrip
-        Dim menu As New ContextMenuStrip()
+    Private Sub ApplyStrategyDefinitionAndAnalyze(strat As StrategyDefinition)
+        If strat Is Nothing Then Return
 
-        Dim mnuInsertIndicator As New ToolStripMenuItem("지표 삽입")
-        AddIndicatorItem(mnuInsertIndicator, "이동평균(MA)", Function() New MA_Indicator(20, "SMA"))
-        AddIndicatorItem(mnuInsertIndicator, "볼린 밴드", Function() New Bollinger_Indicator(20, 2.0F))
-        AddIndicatorItem(mnuInsertIndicator, "거래량 지표", Function() New Volume_Indicator())
-        AddIndicatorItem(mnuInsertIndicator, "MACD", Function() New MACD_Indicator())
-        AddIndicatorItem(mnuInsertIndicator, "RSI", Function() New RSI_Indicator())
-        AddIndicatorItem(mnuInsertIndicator, "SuperTrend", Function() New SuperTrend_Indicator())
+        _chart.ApplyStrategy(strat)
 
-        Dim mnuTech As New ToolStripMenuItem("기술 지표")
-        AddIndicatorItem(mnuTech, "VWAP", Function() New VWAP_Indicator())
-        AddIndicatorItem(mnuTech, "OBV", Function() New OBV_Indicator())
-        AddIndicatorItem(mnuTech, "이격도", Function() New Disparity_Indicator())
-        AddIndicatorItem(mnuTech, "JMA", Function() New JMA_Indicator())
-        AddIndicatorItem(mnuTech, "TickIntensity", Function() New TickIntensity_Indicator())
-        AddIndicatorItem(mnuTech, "체결강도", Function() New TradeStrength_Indicator())
-        AddIndicatorItem(mnuTech, "프로그램 순매수", Function() New ProgramTrade_Indicator())
-        AddIndicatorItem(mnuTech, "누적 거래대금", Function() New CumTradeAmount_Indicator())
-        AddIndicatorItem(mnuTech, "섹터 리더", Function() New SectorLeader_Indicator())
-        mnuInsertIndicator.DropDownItems.Add(New ToolStripSeparator())
-        mnuInsertIndicator.DropDownItems.Add(mnuTech)
-        menu.Items.Add(mnuInsertIndicator)
+        Dim builtIn As IStrategy = BuiltInStrategyFactory.CreateStrategy(strat.Name)
+        If builtIn IsNot Nothing Then
+            RunStrategyAnalysis(builtIn)
+            Return
+        End If
 
-        Dim mnuAutoRoll As New ToolStripMenuItem("자동 롤링")
-        mnuAutoRoll.Enabled = False
-        menu.Items.Add(mnuAutoRoll)
+        AppLogger.I.Info("V2 전략 적용 완료: " & strat.Name, "ChartStrategy")
+    End Sub
 
-        Dim mnuChartManage As New ToolStripMenuItem("차트 관리")
-        Dim mnuLatest As New ToolStripMenuItem("최근봉 이동")
-        AddHandler mnuLatest.Click, Sub() SendKeys.SendWait("{END}")
-        mnuChartManage.DropDownItems.Add(mnuLatest)
+    Private Sub ApplyHardcodedStrategyAndAnalyze(strategy As IStrategy)
+        If strategy Is Nothing Then Return
 
-        Dim mnuShowAll As New ToolStripMenuItem("전체 보기")
-        AddHandler mnuShowAll.Click, Sub() ShowAllCandlesByMenu()
-        mnuChartManage.DropDownItems.Add(mnuShowAll)
+        _chart.ApplyStrategy(strategy)
+        RunStrategyAnalysis(strategy)
+    End Sub
 
-        Dim mnuResetY As New ToolStripMenuItem("Y축 자동 복귀")
-        AddHandler mnuResetY.Click, Sub() ResetYAxisByMenu()
-        mnuChartManage.DropDownItems.Add(mnuResetY)
-        menu.Items.Add(mnuChartManage)
+    Private Sub RunStrategyAnalysis(strategy As IStrategy)
+        If strategy Is Nothing Then Return
 
-        Dim mnuStrategy As New ToolStripMenuItem("전략 삽입")
-        AddHandler mnuStrategy.Click,
-            Sub()
-                Using f As New StrategyManagerForm()
-                    f.ShowDialog(Me)
-                End Using
-            End Sub
-        menu.Items.Add(mnuStrategy)
+        Dim result As ChartStrategyAnalysisResult = ChartStrategyAnalysisService.Run(_chart, strategy)
+        AppLogger.I.Info("V2 전략 분석: " & result.StockCode & " / " & result.StrategyDisplayName & " / 신호 " & result.SignalCount.ToString() & "건 / 거래 " & result.TradeCount.ToString() & "건 / " & result.Message, "ChartStrategy")
 
-        Dim mnuCompare As New ToolStripMenuItem("비교 차트")
-        mnuCompare.Enabled = False
-        menu.Items.Add(mnuCompare)
+        Using f As New StrategyBacktestResultForm(result)
+            f.ShowDialog(Me)
+        End Using
+    End Sub
 
-        Dim mnuChartInit As New ToolStripMenuItem("차트 초기화")
-        AddHandler mnuChartInit.Click,
-            Sub()
-                _chart.ApplyChartProfile(New ChartProfileData())
-                _chart.ReCalculate()
-                SaveProfileNow()
-            End Sub
-        menu.Items.Add(mnuChartInit)
+    Private Sub OnDataViewRequested(sender As Object, e As EventArgs)
+        Dim dataArrays As Dictionary(Of String, Single()) = _chart.GetDataArrays()
+        Dim frm As New frmDataView()
+        frm.SetData(_stockCode, dataArrays)
+        frm.Show(Me)
+    End Sub
 
-        Dim mnuSettings As New ToolStripMenuItem("차트 설정")
-        mnuSettings.Enabled = False
-        menu.Items.Add(mnuSettings)
-
-        Dim mnuSave As New ToolStripMenuItem("차트 상태 저장")
-        AddHandler mnuSave.Click, Sub() SaveProfileNow()
-        menu.Items.Add(mnuSave)
-
-        menu.Items.Add(New ToolStripSeparator())
-
-        Dim mnuClose As New ToolStripMenuItem("차트 닫기")
-        AddHandler mnuClose.Click, Sub() Me.Close()
-        menu.Items.Add(mnuClose)
-
-        Return menu
+    Public Function GetStockName(stockCode As String) As String Implements IChartHost.GetStockName
+        Dim item = StockInfoManager.I.GetItem(stockCode)
+        If item IsNot Nothing Then Return item.Name
+        Return stockCode
     End Function
 
-    Private Sub AddIndicatorItem(parent As ToolStripMenuItem, caption As String, factory As Func(Of IIndicator))
-        Dim item As New ToolStripMenuItem(caption)
-        AddHandler item.Click,
-            Sub()
-                Dim ind As IIndicator = factory.Invoke()
-                If ind Is Nothing Then Return
-                _chart.AddIndicator(ind)
-                _chart.ReCalculate()
-                SaveProfileNow()
-            End Sub
-        parent.DropDownItems.Add(item)
+    Public Sub RequestCandles(stockCode As String, chartType As String, count As Integer) Implements IChartHost.RequestCandles
+        Dim requestCode As String = SharedUtil.NormalizeChartCode(stockCode)
+        If StockInfoManager.I.TryEmitCachedCandles(requestCode, count) Then Return
+        StockInfoManager.I.MarkCandleRequested(requestCode)
+
+        Dim topic As String = Topics.CANDLE_REQUEST
+        If String.Equals(chartType, "day", StringComparison.OrdinalIgnoreCase) Then topic = Topics.DAILY_REQUEST
+
+        MessageBus.I.Emit(topic,
+                          "code", requestCode,
+                          "stockCode", requestCode,
+                          "provider", RuntimeChartSettings.MarketDataProvider,
+                          "timeframe", RuntimeChartSettings.DefaultCandleTimeframe,
+                          "count", count)
     End Sub
 
-    Private Sub SaveProfileNow()
-        ChartProfileService.I.SaveProfile(_chart.ExportChartProfile())
+    Public Sub SubscribeRealtime(stockCode As String) Implements IChartHost.SubscribeRealtime
+        MessageBus.I.Emit(Topics.REALTIME_SUBSCRIBE, "codes", stockCode)
     End Sub
 
-    Private Sub ShowAllCandlesByMenu()
-        Dim stateField As FieldInfo = GetType(SafeFastChartControl).GetField("_state", BindingFlags.Instance Or BindingFlags.NonPublic)
-        Dim bufferField As FieldInfo = GetType(SafeFastChartControl).GetField("_buffer", BindingFlags.Instance Or BindingFlags.NonPublic)
-        Dim repaintMethod As MethodInfo = GetType(SafeFastChartControl).GetMethod("RequestRepaint", BindingFlags.Instance Or BindingFlags.NonPublic)
-
-        Dim state As SafeChartState = TryCast(stateField.GetValue(_chart), SafeChartState)
-        Dim buffer As SafeChartDataBuffer = TryCast(bufferField.GetValue(_chart), SafeChartDataBuffer)
-        If state Is Nothing OrElse buffer Is Nothing Then Return
-        If buffer.Count <= 0 Then Return
-
-        state.StartIndex = 0
-        state.VisibleCount = buffer.Count
-        state.Clamp(buffer.Count)
-        If repaintMethod IsNot Nothing Then repaintMethod.Invoke(_chart, Nothing)
-        SaveProfileNow()
-    End Sub
-
-    Private Sub ResetYAxisByMenu()
-        Dim stateField As FieldInfo = GetType(SafeFastChartControl).GetField("_state", BindingFlags.Instance Or BindingFlags.NonPublic)
-        Dim repaintMethod As MethodInfo = GetType(SafeFastChartControl).GetMethod("RequestRepaint", BindingFlags.Instance Or BindingFlags.NonPublic)
-        Dim state As SafeChartState = TryCast(stateField.GetValue(_chart), SafeChartState)
-        If state Is Nothing Then Return
-
-        state.ResetManualPriceScale()
-        If repaintMethod IsNot Nothing Then repaintMethod.Invoke(_chart, Nothing)
-        SaveProfileNow()
+    Public Sub UnsubscribeRealtime(stockCode As String) Implements IChartHost.UnsubscribeRealtime
+        ' 원본 ChartForm과 동일하게 구독 해제는 매니저 정책에 맡긴다.
     End Sub
 End Class
