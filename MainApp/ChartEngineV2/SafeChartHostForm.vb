ba5f97003660353a74dfc8a38fdc3b2239dc0fcb
hoonoh57 <36817134+hoonoh57@users.noreply.github.com>
2026-05-01 10:35:37 +0900

    Load and save chart profile in V2 host

--- MainApp/ChartEngineV2/SafeChartHostForm.vb
+++ MainApp/ChartEngineV2/SafeChartHostForm.vb
@@ -1,16 +1,24 @@
-﻿Option Strict On
+Option Strict On
 Option Explicit On
 Option Infer Off
 
 Imports System
 Imports System.Windows.Forms
+Imports MainApp.Services
 
 Public Class SafeChartHostForm
     Inherits Form
 
     Private ReadOnly _chart As SafeFastChartControl
+    Private ReadOnly _stockCode As String
+    Private ReadOnly _chartType As String
+    Private ReadOnly _count As Integer
 
     Public Sub New(stockCode As String, Optional chartType As String = "minute", Optional count As Integer = 300)
+        _stockCode = stockCode
+        _chartType = chartType
+        _count = count
+
         Me.Text = "Safe Chart V2 - " & stockCode
         Me.Width = 1200
         Me.Height = 800
@@ -20,9 +28,16 @@ Public Class SafeChartHostForm
         _chart.Dock = DockStyle.Fill
         Me.Controls.Add(_chart)
 
-        AddHandler Me.Shown,
-            Sub()
-                _chart.SetStock(stockCode, chartType, count)
-            End Sub
+        AddHandler _chart.ChartProfileChanged, AddressOf OnChartProfileChanged
+        AddHandler Me.Shown, AddressOf OnSafeChartHostShown
+    End Sub
+
+    Private Sub OnSafeChartHostShown(sender As Object, e As EventArgs)
+        _chart.ApplyChartProfile(ChartProfileService.I.GetProfile())
+        _chart.SetStock(_stockCode, _chartType, _count)
+    End Sub
+
+    Private Sub OnChartProfileChanged(sender As Object, e As EventArgs)
+        ChartProfileService.I.SaveProfile(_chart.ExportChartProfile())
     End Sub
 End Class
